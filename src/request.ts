d3005d994d9c3196ecb83ff0c77a57c30ff83539
chengquanxu <1614177037@qq.com>
2024-06-25 14:54:17 +0800

    修改配置

--- src/request.ts
+++ src/request.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
+// 是否是开发环境
+export const isDev = process.env.NODE_ENV === "development";
+
+// 创建 Axios 实例
 const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
-  timeout: 60000,
-  withCredentials: true,
+    baseURL: isDev ? "http://localhost:8101" : "https://yudada-backend-112474-5-1319341827.sh.run.tcloudbase.com/",
+    timeout: 60000,
+    withCredentials: true,
 });
 
 // 全局请求拦截器
